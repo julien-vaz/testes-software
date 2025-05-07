cae1193fed4e59e76e2032bc9f1da0d8e81d54a7
Julien Hervot de Mattos Vaz <jhmvaz95@gmail.com>
2025-05-07 10:04:34 -0300

    Cria teste case Sensitive Empresa

--- tests/test_criacao_empresa.py
+++ tests/test_criacao_empresa.py
@@ -11,5 +11,12 @@ class TestCriacaoEmpresa(unittest.TestCase):
         with self.assertRaises(ValueError):
             empresa = Empresa('')
 
+    def test_case_sensitive_criar_empresa(self):
+        empresa = Empresa('W')
+
+        value = empresa.getName()
+
+        assert('w' != value)
+
 if __name__ == "__test_criacao_empresa__":
     unittest.main()
