e01566a92c5024814962bd93144889e8d7972c5c
Julien Hervot de Mattos Vaz <jhmvaz95@gmail.com>
2025-05-07 10:26:20 -0300

    Teste de inclusão de projeto em empresa

---
+++ tests/test_inclusao_projeto.py
@@ -0,0 +1,19 @@
+from src.funcionario import Funcionario
+from src.empresa import Empresa
+from src.projeto import Projeto
+import unittest
+
+
+class TestInclusaoProjeto(unittest.TestCase):
+    def setUp(self):
+        self.empresa = Empresa("W")
+
+    def test_inclusao_projeto_backrooms(self):
+        backrooms = Projeto('Backrooms')
+        self.empresa.incluir_projeto(backrooms)
+        assert(len(self.empresa.projetos) == 1)
+
+
+
+if __name__ == "__name__":
+    unittest.main()
