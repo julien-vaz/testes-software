0b438866cbe26d0931dc9db04c80a746e1466c3b
Julien Hervot de Mattos Vaz <jhmvaz95@gmail.com>
2025-05-07 10:22:04 -0300

    Criação teste de projeto vazio

--- tests/test_criacao_projeto.py
+++ tests/test_criacao_projeto.py
@@ -10,6 +10,9 @@ class TestCriacaoProjeto(unittest.TestCase):
         HI = Projeto('Human Instrumentalization')
         assert(HI.nome == 'Human Instrumentalization')
 
+    def test_criacao_projeto_vazio(self):
+        with self.assertRaises(ValueError):
+            HI = Projeto('')
 
 if __name__ == "__test_criacao_projeto__":
     unittest.main()
