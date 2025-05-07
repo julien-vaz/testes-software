246738d2c88cf562ad7c5637034073d84ba2dece
Julien Hervot de Mattos Vaz <jhmvaz95@gmail.com>
2025-05-07 10:11:29 -0300

    inclui Teste de inserção de funcionário duplicado

--- tests/test_inclusao_funcionario.py
+++ tests/test_inclusao_funcionario.py
@@ -12,5 +12,13 @@ class TestInclusaoFuncionario(unittest.TestCase):
         self.empresa.incluir_funcionario(hanako)
         assert(len(self.empresa.funcionarios) == 1)
 
+    def test_insert_same_funcionario(self):
+        hanako = Funcionario('Hanako')
+        self.empresa.incluir_funcionario(hanako)
+        with self.assertRaises(DuplicateFuncionarioException):
+            self.empresa.incluir_funcionario(hanako)
+
+
+
 if __name__ == "__name__":
     unittest.main()
