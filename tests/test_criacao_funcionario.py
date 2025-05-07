0472478f54cd7c51958b4a400ba0adb2494ebbaa
Julien Hervot de Mattos Vaz <jhmvaz95@gmail.com>
2025-05-07 10:07:55 -0300

    Adiciona Teste criação Funcionário Vazio

--- tests/test_criacao_funcionario.py
+++ tests/test_criacao_funcionario.py
@@ -4,8 +4,12 @@ from src.funcionario import Funcionario
 class TestCriacaoFuncionario(unittest.TestCase):
 
     def test_criacao_funcionario_hanako(self):
-        funcionario = Funcionario('Hanako')
-        assert(funcionario.nome == 'Hanako')
+        hanako = Funcionario('Hanako')
+        assert(hanako.nome == 'Hanako')
+
+    def test_criacao_funcionario_vazio(self):
+        with self.assertRaises(ValueError):
+            funcionario = Funcionario('')
 
 if __name__ == "__test_criacao_funcionario__":
     unittest.main()
