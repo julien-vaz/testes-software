df04a973ecdec9f1279ba68c25f336806b4ccdc4
gabriel-turatti <turattigabriel@hotmail.com>
2025-05-14 10:32:25 -0300

    Provando a Enumerabilidade de Testes

--- tests/test_criacao_ocorrencia.py
+++ tests/test_criacao_ocorrencia.py
@@ -15,21 +15,21 @@ class TestCriacaoOcorrencia(unittest.TestCase):
         self.fenn = Funcionario('Fenn')
         self.projeto.incluir_funcionarios([self.joon, self.mary, self.fenn])
 
-    def test_criacao_ocorrencia_seven_bells(self):
+    def test_criacao_ocorrencia_seven_bells(self): # Teste-14
         self.projeto.cria_ocorrencia("Seven Bells for Seven Hells", "Acquire Seven Souls. (0/7)", datetime.date.today(), self.joon)
         newOcorrencia = self.projeto.ocorrencias[-1]
         assert("Seven Bells for Seven Hells" == newOcorrencia.nome)
 
-    def test_criacao_ocorrencia_funcionario_nao_projeto(self):
+    def test_criacao_ocorrencia_funcionario_nao_projeto(self): # Teste-15
         larkspur = Funcionario("Larkspur")
         with self.assertRaises(ValueError):
             self.projeto.cria_ocorrencia("Straddling Worlds", "Consult at the Athenaeum of Speculation and Scrutiny.", datetime.date.today(), larkspur)
 
-    def test_criacao_ocorrencia_sem_nome(self):
+    def test_criacao_ocorrencia_sem_nome(self): # Teste-16
         with self.assertRaises(ValueError):
             self.projeto.cria_ocorrencia("", "[REDACTED]", datetime.date.today(), self.joon)
 
-    def test_criacao_ocorrencia_ids_funcionando(self):
+    def test_criacao_ocorrencia_ids_funcionando(self): # Teste-17
         self.projeto.cria_ocorrencia("God Botherer", "Meet with a god.", datetime.date.today(), self.joon)
         ocorrenciaGod = self.projeto.ocorrencias[-1]
         self.projeto.cria_ocorrencia("Comfort Zone", "Leave the city of Comfort.", datetime.date.today(), self.joon)
@@ -37,7 +37,7 @@ class TestCriacaoOcorrencia(unittest.TestCase):
 
         self.assertNotEqual(ocorrenciaGod.id, ocorrenciaComfort.id)
 
-    def test_criacao_funcionario_mais_de_10_ocorrencias(self):
+    def test_criacao_funcionario_mais_de_10_ocorrencias(self): # Teste-18
         self.projeto.cria_ocorrencia("Seven Bells for Seven Hells", "Acquire Seven Souls. (0/7)", datetime.date.today(), self.joon)
         self.projeto.cria_ocorrencia("Straddling Worlds", "Consult at the Athenaeum of Speculation and Scrutiny.", datetime.date.today(), self.joon)
         self.projeto.cria_ocorrencia("God Botherer", "Meet with a god.", datetime.date.today(), self.joon)
@@ -53,39 +53,39 @@ class TestCriacaoOcorrencia(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.projeto.cria_ocorrencia("Boneitis", "You used up the magic in your bones, and it's not going to come back, find a way to replenish it.", datetime.date.today(), self.joon)
 
-    def test_criacao_ocorrencia_fechada(self):
+    def test_criacao_ocorrencia_fechada(self): # Teste-19
         self.projeto.cria_ocorrencia("Heading Off the Skin Mage", "Defeat Leonold before he kills you.", datetime.date.today(), self.fenn)
         ocorrencia = self.projeto.ocorrencias[-1]
         self.fenn.fecha_ocorrencia(ocorrencia)
 
         self.assertEqual(False, ocorrencia.status)
 
-    def test_criacao_ocorrencia_fechada_nao_autorizado(self):
+    def test_criacao_ocorrencia_fechada_nao_autorizado(self): # Teste-20
         self.projeto.cria_ocorrencia("Summer's End", "Return to the place where Fenn received her scars and bring justice to the elves.", datetime.date.today(), self.fenn)
         ocorrencia = self.projeto.ocorrencias[-1]
         with self.assertRaises(ValueError):
             self.mary.fecha_ocorrencia(ocorrencia)
 
-    def test_modifica_responsavel_por_ocorrencia(self):
+    def test_modifica_responsavel_por_ocorrencia(self): # Teste-21
         self.projeto.cria_ocorrencia("Unihorn", "Travel to the Aon Adharc Glen and kill a unicorn.", datetime.date.today(), self.fenn)
         ocorrencia = self.projeto.ocorrencias[-1]
         self.projeto.altera_responsavel(ocorrencia, self.mary)
 
-    def test_modifica_responsavel_por_ocorrencia_projeto_invalido(self):
+    def test_modifica_responsavel_por_ocorrencia_projeto_invalido(self): # Teste-22
         self.projeto.cria_ocorrencia("They Say You Can't Go Home Again", "Find out about the life your body had before you.", datetime.date.today(), self.fenn)
         newProjeto = Projeto("Book II")
         ocorrencia = self.projeto.ocorrencias[-1]
         with self.assertRaises(ValueError):
             newProjeto.altera_responsavel(ocorrencia, self.joon)
 
-    def test_modifica_responsavel_por_ocorrencia_funcionario_de_fora(self):
+    def test_modifica_responsavel_por_ocorrencia_funcionario_de_fora(self): # Teste-23
         grak = Funcionario("Grakhuil")
         self.projeto.cria_ocorrencia("All That Glitters", "Return to Darili Irid with Grakhuil once da has gathered enough gold to statisfy da nad self-imposed penance to da nad former clan.", datetime.date.today(), self.mary)
         ocorrencia = self.projeto.ocorrencias[-1]
         with self.assertRaises(ValueError):
             self.projeto.altera_responsavel(ocorrencia, grak)
 
-    def test_modifica_responsavel_por_ocorrencia_fechada(self):
+    def test_modifica_responsavel_por_ocorrencia_fechada(self): # Teste-24
         doe = Funcionario("Six-Eyed-Doe")
         self.projeto.incluir_funcionarios([doe])
         self.projeto.cria_ocorrencia("Gone to Seed", "There is a place on Aerb considered worse than the first four thousand hells. Defeat Fel Seed.", datetime.date.today(), doe)
